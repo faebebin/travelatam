559264bda9b94eaf920d4e0c9039b6f628afbbd6
faebebin <fabianbndr@gmail.com>
2022-10-11 17:28:03 -0500

    Add dev package view-plugin-mkcert to enable brower cache

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
-export default {
+import { defineConfig } from 'vite'
+import mkcert from 'vite-plugin-mkcert'
+
+// https://vitejs.dev/config/
+export default defineConfig({
   build: {
     sourcemap: true,
   },
-  base: ""
-}
+  base: "",
+  server: {
+    https: true
+  },
+  plugins: [mkcert()]
+})
+
