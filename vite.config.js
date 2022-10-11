c6b8eb601cb74793eee4f15a00f6228cb9094605
faebebin <fabianbndr@gmail.com>
2022-10-11 17:42:06 -0500

    Revert: Add dev package view-plugin-mkcert (remove package)

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
-import { defineConfig } from 'vite'
-import mkcert from 'vite-plugin-mkcert'
-
-// https://vitejs.dev/config/
-export default defineConfig({
+export default {
   build: {
     sourcemap: true,
   },
-  base: "",
-  server: {
-    https: true
-  },
-  plugins: [mkcert()]
-})
-
+  base: ""
+}
