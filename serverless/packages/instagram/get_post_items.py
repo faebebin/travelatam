9da0f7350ddb5b787c3e464109def943291d3ad8
faebebin <fabianbndr@gmail.com>
2022-10-28 13:02:28 -0500

    Complete do serverless api proxy

--- serverless/packages/instagram/get_post_items.py
+++ serverless/packages/instagram/get_post_items.py
@@ -4,9 +4,10 @@ import urllib.request
 def main(args):
     fields = 'media_type,media_url'
     access_token = os.getenv('INSTA_API_TOKEN')
-    media_id = args.get("media_id")
-    url = f"https://graph.instagram.com/{media_id}/children?fields=${fields}&access_token={access_token}"
-    print(url)
+    media_id = args.get("media_id", "MISSING_media_id")
+    # https://developers.facebook.com/docs/instagram-basic-display-api/reference/media/children#reading
+    url = f"https://graph.instagram.com/{media_id}/children?fields={fields}&access_token={access_token}"
+    # print(url)
     with urllib.request.urlopen(url) as response:
         json_bytes = response.read()
     return {"body": json_bytes.decode('utf-8')}
--- serverless/packages/instagram/get_posts.py
+++ serverless/packages/instagram/get_posts.py
@@ -4,8 +4,9 @@ import urllib.request
 def main(args):
     fields = 'id,caption,media_type,media_url,timestamp'
     access_token = os.getenv('INSTA_API_TOKEN')
+    # https://developers.facebook.com/docs/instagram-basic-display-api/reference/user/media#reading
     url = f"https://graph.instagram.com/me/media?fields={fields}&access_token={access_token}"
-    print(url)
+    # print(url)
     with urllib.request.urlopen(url) as response:
         json_bytes = response.read()
     return {"body": json_bytes.decode('utf-8')}
